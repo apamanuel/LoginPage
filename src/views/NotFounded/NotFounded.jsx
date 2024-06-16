078936450cc86c6f045be323f88ac84f3ede5a15
ManuelE <apamanuel@gmail.com>
2024-06-16 13:31:04 -0400

    Styles in NotFounded

--- src/views/NotFounded/NotFounded.jsx
+++ src/views/NotFounded/NotFounded.jsx
@@ -1,5 +1,7 @@
+import style from "./NotFounded.module.css";
+
 const NotFounded = () => {
-  return <h1>Pagina no encontrada!!</h1>;
+  return <h1 className={style.container}>Page not found!!</h1>;
 };
 
 export default NotFounded;
